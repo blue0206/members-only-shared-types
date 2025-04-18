48d5745f8a51417f0a886d1c4c92d2c1e94e074a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-18 17:24:44 +0530

    refactor: enhance validation messages in Register/Login request schemas

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -4,23 +4,31 @@ import { UserSchema } from './user.dto.js';
 // REGISTER REQUEST
 // Schema
 export const RegisterRequestSchema = z.object({
-    username: z.string().regex(/^[a-zA-Z0-9_]+$/),
-    firstname: z.string().regex(/^[a-zA-Z]+$/),
+    username: z.string().regex(/^[a-zA-Z0-9_]+$/, {
+        message: 'The username can only have letters, numbers, and underscores.',
+    }),
+    firstname: z
+        .string()
+        .regex(/^\p{L}+(-\p{L}+)*$/u, { message: 'The first name is invalid.' }),
     middlename: z
         .string()
-        .regex(/^[a-zA-Z]+$/)
+        .regex(/^\p{L}+(-\p{L}+)*$/u, { message: 'The middle name is invalid.' })
         .optional(),
     lastname: z
         .string()
-        .regex(/^[a-zA-Z]+$/)
+        .regex(/^\p{L}+(-\p{L}+)*$/u, { message: 'The last name is invalid.' })
         .optional(),
     password: z
         .string()
         .min(8)
         .regex(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            {
+                message:
+                    'The password must contain at least 8 characters with at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+            }
         ),
-    avatar: z.string().url().optional(),
+    avatar: z.string().url({ message: 'The avatar must be a valid URL' }).optional(),
 });
 // DTO
 export type RegisterRequestDto = z.infer<typeof RegisterRequestSchema>;
@@ -38,12 +46,18 @@ export type RegisterResponseDto = z.infer<typeof RegisterResponseSchema>;
 // LOGIN REQUEST
 // Schema
 export const LoginRequestSchema = z.object({
-    username: z.string().regex(/^[a-zA-Z0-9_]+$/),
+    username: z.string().regex(/^[a-zA-Z0-9_]+$/, {
+        message: 'The username can only have letters, numbers, and underscores.',
+    }),
     password: z
         .string()
         .min(8)
         .regex(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            {
+                message:
+                    'The password must contain at least 8 characters with at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+            }
         ),
 });
 // DTO
