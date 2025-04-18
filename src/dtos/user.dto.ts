294abc043bf76110732d43de76f09ea7329a2bc7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-18 18:22:35 +0530

    feat: add user-specific message response schema and DTO

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { Role } from '../enums/roles.enum.js';
+import { GetMessagesResponseSchema } from './message.dto.js';
 
 // User Schema
 export const UserSchema = z.object({
@@ -13,3 +14,16 @@ export const UserSchema = z.object({
 });
 // User DTO
 export type UserDto = z.infer<typeof UserSchema>;
+
+// GET MESSAGES
+// Response Schema
+export const GetUserMessagesResponseSchema = GetMessagesResponseSchema;
+// Response DTO
+export type GetUserMessagesResponseDto = z.infer<
+    typeof GetUserMessagesResponseSchema
+>;
+// Note: Though the schema is same, the use case is different.
+// In this case, the messages fetched only belong to the user unlike with
+// the generic schema imported which is for all messages.
+// This schema will be used in User route, for a clear separation of concerns,
+// with the redeclaration of variable.
