4d87d9e925ca31d57d6c5fa43f7f3cb6a081c16f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-18 17:52:38 +0530

    feat: add response schema and DTO for Get Messages with author

--- src/dtos/message.dto.ts
+++ src/dtos/message.dto.ts
@@ -13,3 +13,16 @@ export const GetMessagesWithoutAuthorResponseSchema = z.array(
 export type GetMessagesWithoutAuthorResponseDto = z.infer<
     typeof GetMessagesWithoutAuthorResponseSchema
 >;
+
+// GET MESSAGES (Member/Admin)
+// Response Schema
+export const GetMessagesResponseSchema = z.array(
+    z.object({
+        messageId: z.number(),
+        message: z.string(),
+        username: z.string(),
+        timestamp: z.date(),
+    })
+);
+// Response DTO
+export type GetMessagesResponseDto = z.infer<typeof GetMessagesResponseSchema>;
